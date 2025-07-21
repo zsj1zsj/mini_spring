af1c30d5d9f2b18adb11cb709d5db8aee3bb7d96
Lynn Zhang <zsj1zsj@gmail.com>
2025-07-22 07:52:30 +0800

    tomcat test

--- src/main/java/tech/insight/spring/Main.java
+++ src/main/java/tech/insight/spring/Main.java
@@ -1,11 +1,43 @@
 package tech.insight.spring;
 
 
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import tech.insight.spring.test.HelloServlet;
+
+import java.io.File;
+import java.util.logging.LogManager;
+
 /**
  * @author gongxuanzhangmelt@gmail.com
  **/
 public class Main {
     public static void main(String[] args) throws Exception {
-        new ApplicationContext("tech.insight.spring");
+//        new ApplicationContext("tech.insight.spring");
+        tomcat();
+    }
+
+
+
+    public static void tomcat() throws LifecycleException {
+//        LogManager.getLogManager().reset();
+//        SLF4JBridgeHandler.removeHandlersForRootLogger();
+//        SLF4JBridgeHandler.install();
+        int port = 8080;
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(port);
+        tomcat.getConnector();
+
+        String contextPath = "";
+        String docBase = new File(".").getAbsolutePath();
+        Context context = tomcat.addContext(contextPath, docBase);
+        HelloServlet helloServlet = new HelloServlet();
+
+        tomcat.addServlet(contextPath, "helloServlet", helloServlet);
+        context.addServletMappingDecoded("/*", "helloServlet");
+        tomcat.start();
+        System.out.println("tomcat start...  port :" + port);
     }
 }
---
+++ src/main/java/tech/insight/spring/test/HelloServlet.java
@@ -0,0 +1,32 @@
+package tech.insight.spring.test;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    private String message;
+
+    public void init() throws ServletException {
+        // Do required initialization
+        message = "Hello World";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Set response content type
+        response.setContentType("text/html");
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+
+    public void destroy() {
+        // do nothing.
+    }
+}
